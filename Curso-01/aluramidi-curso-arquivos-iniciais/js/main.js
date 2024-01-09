3e24668b8c60e939abee3060d24d4e41b17ad204
murilojcavalcanti <josemurilo7@gmail.com>
2024-01-09 00:41:14 -0300

    primeiras aulas do curso

---
+++ Curso-01/aluramidi-curso-arquivos-iniciais/js/main.js
@@ -0,0 +1,44 @@
+function tocaSomPom(){
+    document.querySelector("#som_tecla_pom").play();   
+}
+document.querySelector(".tecla_pom").onclick = tocaSomPom; 
+
+function tocaSomClap(){
+    document.querySelector("#som_tecla_clap").play();   
+}
+document.querySelector(".tecla_clap").onclick = tocaSomClap; 
+
+function tocaSomTim(){
+    document.querySelector("#som_tecla_tim").play();   
+}
+document.querySelector(".tecla_tim").onclick = tocaSomTim;
+
+function tocaSomPuff(){
+    document.querySelector("#som_tecla_puff").play();   
+}
+document.querySelector(".tecla_puff").onclick = tocaSomPuff; 
+
+function tocaSomsplash(){
+    document.querySelector("#som_tecla_splash").play();   
+}
+document.querySelector(".tecla_splash").onclick = tocaSomsplash; 
+
+function tocaSomToim(){
+    document.querySelector("#som_tecla_toim").play();   
+}
+document.querySelector(".tecla_toim").onclick = tocaSomToim;
+
+function tocaSomPsh(){
+    document.querySelector("#som_tecla_psh").play();   
+}
+document.querySelector(".tecla_psh").onclick = tocaSomPsh; 
+
+function tocaSomtic(){
+    document.querySelector("#som_tecla_tic").play();   
+}
+document.querySelector(".tecla_tic").onclick = tocaSomtic; 
+
+function tocaSomTom(){
+    document.querySelector("#som_tecla_tom").play();   
+}
+document.querySelector(".tecla_tom").onclick = tocaSomTom;
